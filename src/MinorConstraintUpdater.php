e79daaa7b738a7102e315c122eb098e83d78f855
MartinsRucevskis <93941023+MartinsRucevskis@users.noreply.github.com>
2023-11-15 13:54:23 +0200

    Refactored code, added to use default composer json and lock files if possible (#4)
    Co-authored-by: Martins Rucevskis <martins.rucevskis@gmail.com>

--- src/MinorConstraintUpdater.php
+++ src/MinorConstraintUpdater.php
@@ -3,6 +3,7 @@
 namespace MartinsR\ComposerConstraintUpdater;
 
 use Composer\Command\BaseCommand;
+use Composer\Factory;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,12 +13,22 @@ class MinorConstraintUpdater extends BaseCommand
 
     protected function configure()
     {
-        // @codingStandardsIgnoreStart
         $this
             ->setName('minor-update')
             ->setDescription('Will run composer update and replace versions in your composer.json file to the ones inside lock file')
             ->setDefinition([
-                new InputOption('composer-path', 'C', InputOption::VALUE_REQUIRED, 'Composer json file location'),
+                new InputOption(
+                    'composer-json',
+                    null,
+                    InputOption::VALUE_OPTIONAL,
+                    'Composer json file location'
+                ),
+                new InputOption(
+                    'composer-lock',
+                    null,
+                    InputOption::VALUE_OPTIONAL,
+                    'Composer lock file location'
+                ),
             ])
             ->setHelp(<<<EOT
             The <info>minor-update</info> command executes a composer update, which updates your composer.json
@@ -25,17 +36,21 @@ class MinorConstraintUpdater extends BaseCommand
             This means it will update the versions of your packages to the latest minor versions, while respecting the version constraints defined in your composer.json file.
             EOT
             );
-        // @codingStandardsIgnoreEnd
     }
 
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $composerPath = $input->getOption('composer-path');
+        $composerPath = $input->getOption('composer-json') ?? Factory::getComposerFile();
+        $composerLock = $input->getOption('composer-lock') ?? Factory::getLockFile($composerPath);
 
+        $output->writeln('Launching composer update');
         $this->updateComposer();
-        file_put_contents($composerPath, $this->versionsFromLock($composerPath));
 
+        $output->writeln('Rebuilding composer.json from lock file');
+        file_put_contents($composerPath, (new ComposerJsonFromLockBuilder($composerPath, $composerLock))->versionsFromLock());
+
+        $output->writeln('Composer.json has been successfully updated!');
         return 1;
     }
 
@@ -44,61 +59,5 @@ class MinorConstraintUpdater extends BaseCommand
         $update = shell_exec('composer update');
 
         echo $update;
-    }
-
-    public function versionsFromLock(string $composerPath)
-    {
-        $composerLockPath = str_replace('composer.json', 'composer.lock', $composerPath);
-
-        $composerLockContents = file_get_contents($composerLockPath);
-        $composerJsonContents = file_get_contents($composerPath);
-
-        $types = ['require' => 'packages', 'require-dev' => 'packages-dev'];
-        foreach ($types as $typeJson => $typeLock) {
-            preg_match('/"' . preg_quote($typeJson) . '":\s+{([\s\S]+?)}/', $composerJsonContents, $dependencies);
-            $dependencies = $this->processDependencies(preg_split('/,/ms', $dependencies[1]));
-            foreach ($dependencies as $dependencyName => $version) {
-                preg_match('#"name": ' . preg_quote($dependencyName) . ',\s+"version": "(.+)"#m', $composerLockContents, $match);
-                if (isset($match[1])) {
-                    echo  $dependencyName . ': ' . $version;
-                    $composerJsonContents = str_replace($dependencyName . ': ' . $version, $dependencyName . ': "^' . $match[1] . '"', $composerJsonContents);
-                }
-            }
-        }
-
-        return $composerJsonContents;
-    }
-
-    public
-    function replaceVersions(string $composerContents, array $packageConstraints): string
-    {
-        $requirements = ['require', 'require-dev'];
-        foreach ($requirements as $requirement) {
-            preg_match('/"' . preg_quote($requirement) . '"\s*:\s*\{([\s|\S]+?)\}/m', $composerContents, $matches);
-            $packages = preg_replace('/:\s*"(.*)+?"/m', ': "*"', $matches[1]);
-            foreach ($packageConstraints as $constraint => $version) {
-                $packages = preg_replace(
-                    '@"' . preg_quote($constraint) . '"\s*:\s*"\*"@m',
-                    '"' . preg_quote($constraint) . '": "' . $version . '"',
-                    $packages
-                );
-            }
-            $composerContents = preg_replace('/"' . preg_quote($requirement) . '"\s*:\s*\{([\s|\S]+?)\}/m', '"' . $requirement . '": {' . $packages . '}', $composerContents);
-        }
-
-        return $composerContents;
-    }
-
-    private
-    function processDependencies(array $dependencies): array
-    {
-        $composerDependencies = [];
-        foreach ($dependencies as &$dependency) {
-            $dependency = preg_replace('/\s+/ms', '', $dependency);
-            $dependency = explode(':', $dependency);
-            $composerDependencies[$dependency[0]] = $dependency[1];
-        }
-
-        return $composerDependencies;
     }
 }
